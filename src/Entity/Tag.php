abd267dd6bf0725674f3a380fc76c10574fd6a70
Jessica Salvat <jsalvat7@gmail.com>
2019-07-04 21:56:25 +0200

    add bootswatch theme

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
@@ -20,7 +21,6 @@ class Tag
 
     /**
      * @Assert\Length(max=255)
-     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $name;
