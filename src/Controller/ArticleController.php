0e32d7bf88e46177ce0befac2b14f9eef54fce0e
Ryan Vernex <ryanvernex@yahoo.fr>
2019-11-30 12:46:28 +0100

    Page show 1 article + injection dependancies

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,19 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class ArticleController extends AbstractController
 {
     /**
      * @Route("/article", name="article")
      */
-    public function index()
+    public function index(ArticleRepository $repository)
     {
-        $repository = $this->getDoctrine()->getRepository(Article::class);
         $articles = $repository->findAll();
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/article/{id}", name="show_article")
+     */
+    public function showArticle(Article $article){
+        return $this->render('article/show.html.twig',[
+            'article' => $article
         ]);
     }
 }
