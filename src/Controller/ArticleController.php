bd3d9f8c853c5512a88d7d5cfc35cc18b1af3868
Ryan Vernex <ryanvernex@yahoo.fr>
2019-12-01 15:58:42 +0100

    1st form : form add article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleController extends AbstractController
 {
@@ -18,6 +23,25 @@ class ArticleController extends AbstractController
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/article/add", name="add_article")
+     */
+    public function addArticle(Request $request){
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                    ->add('title', TextType::class)
+                    ->add('content', TextAreaType::class)
+                    ->add('image', TextType::class)
+                    ->add('submit', SubmitType::class, [
+                        'label' => 'Add article'
+                    ])
+                    ->getForm();
+
+
+        return $this->render('article/add.html.twig', [
+            'formArticle' => $form->createView()
         ]);
     }
     /**
