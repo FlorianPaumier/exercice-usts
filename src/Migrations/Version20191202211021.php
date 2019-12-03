f021da2229170dd6566037bc15a51517c9df1f88
Ryan Vernex <ryanvernex@yahoo.fr>
2019-12-03 13:13:23 +0100

    create Join Table Article/Tag

--- src/Migrations/Version20191202211021.php
+++ src/Migrations/Version20191202211021.php
@@ -23,6 +23,9 @@ final class Version20191202211021 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_tag (article_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_66CF3FA885C4074C (article_id), INDEX IDX_66CF3FA8A76ED395 (tag_id), PRIMARY KEY(article_id, tag_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE article_tag ADD CONSTRAINT FK_66CF3FA885C4074C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE article_tag ADD CONSTRAINT FK_66CF3FA8A76ED395 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
